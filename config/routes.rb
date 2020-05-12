9d0df3b112d8ea3af0d4415fa83f91082e47984f
kokkedx <55970331+kokkedx@users.noreply.github.com>
2020-05-12 16:31:16 +0900

    sign-up-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'items#new'
+  devise_for :users
+  # root 'items#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
 end
