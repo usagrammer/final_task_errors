36af9a9025aedd4592602c0f4659dc60b9677c6e
nikaido_wataru <58378273+WAKUZO@users.noreply.github.com>
2020-05-13 16:38:39 +0900

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :item_params, only: :create
 
   def index
-    @items = Item.all
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
