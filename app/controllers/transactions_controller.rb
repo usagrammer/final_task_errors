5be62bd0028121ea45e4cc708bdc9d7829cf44f4
masajob <masajobsenyo@gmail.com>
2020-05-19 19:11:34 +0900

    pay jpの記述

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,27 +7,47 @@ class TransactionsController < ApplicationController
 
 
   def create
-    binding.pry
     @item = Item.find(params[:item_id])
-    
+    pay_item
     @transaction = Transaction.new(transaction_params)
   end
 
-
-
   private
   def transaction_params
-    params.require(:transaction).permit(:item_id, :token).merge(user_id: current_user.id)
+    params.permit(:item_id).merge(user_id: current_user.id)
   end
 
+  def pay_item
+    Payjp.api_key = 'sk_test_a309a0a09c01fc5695e76319'
+    # Payjp.api_key = ENV[:payjp_key]
+    # Payjp.open_timeout = 30 # optionally
+    # Payjp.read_timeout = 90 # optionally
 
-  # def payjp
-  #   Payjp.charges.create({
-  #       pk_test_530d79abed1768b5d8674052
-  #       card: "payjpToken",
-  #       amount: 3000,
-  #       currency: "jpy",
-  #     });
-  # end
+    card = {
+      :card => {
+        :number => params[:number],
+        :cvc =>  params[:cvc],
+        :exp_month =>  params[:exp_month],
+        :exp_year =>  '20'+params[:exp_year]
+      }
+    }
 
+    result = Payjp::Token.create(
+      card,
+      {
+        # テスト目的のトークン作成
+        # テスト等の目的でトークンの作成処理をサーバーサイドで完結させたい場合、HTTPヘッダーに X-Payjp-Direct-Token-Generate: true を指定して本APIをリクエストすることで、カード情報を直接指定してトークンを作成することができます。この機能はテストモードでのみ利用可能です。
+        'X-Payjp-Direct-Token-Generate': 'true'
+      }
+    )
+
+    token = result.id
+
+    # ex, create charge
+    charge = Payjp::Charge.create(
+      :amount => @item.price,
+      :card => token,
+      :currency => 'jpy',
+    )
+  end
 end
