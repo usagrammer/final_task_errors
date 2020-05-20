9c640bea9df293f917e4bc2921d903cb2ae5614a
kokkedx <55970331+kokkedx@users.noreply.github.com>
2020-05-20 10:48:02 +0900

    devise-edit

--- db/migrate/20200513072635_remove_author_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAuthorFromUsers < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :users, :birthday_year_id, :string
-    remove_column :users, :birthday_month_id, :string
-    remove_column :users, :birthday_date_id, :string
-  end
-end
