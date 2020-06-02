10490cac09c788e635ddf9bfddb363d94913e04c
masajob <masajobsenyo@gmail.com>
2020-06-02 11:15:04 +0900

    修正追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
   const path = location.pathname
   const pathRegex = /^(?=.*item)(?=.*edit)/
-  if (path === "/items/new" || pathRegex.test(path)) {
+  if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
+    //    出品ページの場合　||　出品ページの検証にかかった場合 || 商品編集の場合
     const priceInput = document.getElementById("item_price");
     const addTaxDom = document.getElementById("add-tax-price");
     const profitDom = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
+      const inputRegex = /^([1-9]\d*|0)$/
       const inputValue = document.getElementById("item_price").value;
-      addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
-      profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+      if (inputRegex.test(inputValue)) {
+        addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+        profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+      } else {
+        addTaxDom.innerHTML = '半角数字のみ入力可能'
+        profitDom.innerHTML = '半角数字のみ入力可能'
+      }
     })
   }
 });
