4f2359f521128c7e33fb6c4847f4c779a2ba04c0
masajob <masajobsenyo@gmail.com>
2020-06-10 16:41:13 +0900

    ユーザーの名前のフリガナへカタカナのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
+  end
+
+  # 全角のカタカナを使用していないか検証
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナを使用してください" } do
     validates :first_name_kana
     validates :last_name_kana
   end
