7d4999f50b186556df47875b1595951cdee012eb
GitHubNaoki <70kio18.rf@gmail.com>
2020-08-07 11:30:35 +0900

    item_price.jsのid名修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,13 +3,13 @@ window.addEventListener("DOMContentLoaded", () => {
   const pathRegex = /^(?=.*item)(?=.*edit)/
   if (path === "/items/new" || path === "/items" || pathRegex.test(path)) {
     //    出品ページの場合　||　出品ページの検証にかかった場合 || 商品編集の場合
-    const priceInput = document.getElementById("item_price");
+    const priceInput = document.getElementById("item-price");
     const addTaxDom = document.getElementById("add-tax-price");
     const profitDom = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
       const inputRegex = /^([1-9]\d*|0)$/
-      const inputValue = document.getElementById("item_price").value;
+      const inputValue = document.getElementById("item-price").value;
       if (inputRegex.test(inputValue)) {
         addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
         profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
