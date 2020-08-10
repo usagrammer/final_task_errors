8b988d870fe639a5554df78dcd5956f3d53e27bb
GitHubNaoki <70kio18.rf@gmail.com>
2020-08-10 12:34:36 +0900

    jsから正規表現による条件分岐を取り除いた

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -8,15 +8,9 @@ window.addEventListener("DOMContentLoaded", () => {
     const profitDom = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
-      const inputRegex = /^([1-9]\d*|0)$/
       const inputValue = document.getElementById("item-price").value;
-      if (inputRegex.test(inputValue)) {
         addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
         profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
-      } else {
-        addTaxDom.innerHTML = ''
-        profitDom.innerHTML = ''
-      }
     })
   }
 });
