c0d6d3e3d43785ba55683b4be73684f5d26bf7b6
ratovia <ratovia@hotmail.co.jp>
2020-10-19 14:19:15 +0900

    【追加実装】コメント機能を実装しました

--- app/models/item.rb
+++ app/models/item.rb
@@ -38,5 +38,6 @@ class Item < ApplicationRecord
 
   # <<アソシエーション>>
   belongs_to :user
+  has_many :comments
   has_one :item_transaction
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_015734) do
+ActiveRecord::Schema.define(version: 2020_10_17_115646) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2020_08_07_015734) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "item_transaction_id", null: false
     t.index ["item_transaction_id"], name: "index_addresses_on_item_transaction_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "item_transactions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
