f635cd750e4ab8b37a000434c414f4ee7f2205c8
ratovia <ratovia@hotmail.co.jp>
2020-10-19 16:57:07 +0900

    fix comments_channel.js

--- app/javascript/channels/comments_channel.js
+++ app/javascript/channels/comments_channel.js
@@ -1,33 +1,34 @@
 import consumer from "./consumer"
+if((location.pathname.match(/items/) && location.pathname.match(/\d+/))){
+  consumer.subscriptions.create({
+    channel: "CommentsChannel",
+    item_id: location.pathname.match(/\d+/)[0]
+  }, {
+    connected() {
+      // ActionCableと通信を確立した際にコンソールにインフォメーションを出力
+      console.info("connected channnel")
+    },
 
-consumer.subscriptions.create({
-  channel: "CommentsChannel",
-  item_id: location.pathname.match(/\d+/)[0]
-}, {
-  connected() {
-    // ActionCableと通信を確立した際にコンソールにインフォメーションを出力
-    console.info("connected channnel")
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    // data =>  {
-    //   "comment"=>{"id"=>72, "content"=>"aaaa", "user_id"=>2, "item_id"=>1, "created_at"=>"2020-08-20T07:54:25.909Z", "updated_at"=>"2020-08-20T07:54:25.909Z"}, 
-    //   "user"=>{"id"=>2, "nickname"=>"yamada", "email"=>"yamada@taro", "first_name"=>"山田", "last_name"=>"太郎", "first_name_kana"=>"ヤマダ", "last_name_kana"=>"タ...}
-    // }
-    console.info("recieved data")
-    console.log(data)
-    const html = `
-      <div class="comment">
-        <div class="user-info">${data.user.nickname}</div>
-        <p>${data.comment.content}</p>
-       </div>
-    `
-    const comments = document.getElementById("comments")
-    comments.insertAdjacentHTML('beforeend', html)
-    comments.scrollTop = comments.scrollHeight;
-  }
-}) 
+    received(data) {
+      // data =>  {
+      //   "comment"=>{"id"=>72, "content"=>"aaaa", "user_id"=>2, "item_id"=>1, "created_at"=>"2020-08-20T07:54:25.909Z", "updated_at"=>"2020-08-20T07:54:25.909Z"}, 
+      //   "user"=>{"id"=>2, "nickname"=>"yamada", "email"=>"yamada@taro", "first_name"=>"山田", "last_name"=>"太郎", "first_name_kana"=>"ヤマダ", "last_name_kana"=>"タ...}
+      // }
+      console.info("recieved data")
+      console.log(data)
+      const html = `
+        <div class="comment">
+          <div class="user-info">${data.user.nickname}</div>
+          <p>${data.comment.content}</p>
+        </div>
+      `
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML('beforeend', html)
+      comments.scrollTop = comments.scrollHeight;
+    }
+  }) 
+}
